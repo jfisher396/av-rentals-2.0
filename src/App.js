931590756590763dac6c13591f490d17b04dc14e
James Fisher <jfisher396@hotmail.com>
2021-04-26 16:44:31 -0700

    navbar built and page routing done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from "./components/Navbar/Navbar"
 
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
-        <Route path="/cart">
+        <Route path="/projection">
+          <h1>This is the projection page</h1>
+        </Route>
+        <Route path="/audio">
+          <h1>This is the audio page</h1>
+        </Route>
+        <Route path="/computers">
+          <h1>This is the computers page</h1>
+        </Route>
+        <Route path="/screens">
+          <h1>This is the screens page</h1>
+        </Route>
+        <Route path="/misc">
+          <h1>This is the misc page</h1>
+        </Route>
+         <Route path="/cart">
           <h1>This is the cart page</h1>
         </Route>
         <Route exact path="/">
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand" href="#">AV Rentals</Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/projection" className="nav-link active" aria-current="page">Projectors</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/screens" className="nav-link active" aria-current="page">Screens</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/audio" className="nav-link active" aria-current="page">Audio</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/misc" className="nav-link active" aria-current="page">Misc AV</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/cart" className="nav-link active" aria-current="page">Cart</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}

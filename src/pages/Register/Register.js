97cb2cc550bc68a48874cc913f11a4e4686b2335
James Fisher <jfisher396@hotmail.com>
2021-05-14 09:18:55 -0700

    container and form added to register page

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import Container from "../../components/Container/Container";
 
 class Register extends Component {
     render() {
         return (
             <div>
-                <h1>Registration page</h1>
+                <Container>
+                    <form>
+                        <div className="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div className="mb-3">
+                            <label for="exampleInputPassword1" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                        </div>
+                        
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </Container>
+                
             </div>
         )
     }

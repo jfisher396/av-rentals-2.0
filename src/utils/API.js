3d4323150056e3630f8d389aa08cc770ec92f4aa
James Fisher <jfisher396@hotmail.com>
2021-05-14 17:22:31 -0700

    url encoded

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,36 +1,39 @@
+
 const axios = require("axios");
+
+const urlPrefix = window.env.API_URL_PREFIX;
 
 const API = {
   getAllProjectors: function () {
-    return axios.get("http://localhost:8080/api/projectors");
+    return axios.get(`${urlPrefix}/api/projectors`);
   },
 
   getAllScreens: function () {
-    return axios.get("http://localhost:8080/api/screens");
+    return axios.get(`${urlPrefix}/api/screens`);
   },
 
   getAllComputers: function () {
-    return axios.get("http://localhost:8080/api/computers");
+    return axios.get(`${urlPrefix}/api/computers`);
   },
 
   getAllAudio: function () {
-    return axios.get("http://localhost:8080/api/audio");
+    return axios.get(`${urlPrefix}/api/audio`);
   },
   
   getAllMisc: function () {
-    return axios.get("http://localhost:8080/api/misc");
+    return axios.get(`${urlPrefix}/api/misc`);
   },
 
   newUser: function (userData) {
-    return axios.post("http://localhost:8080/api/users/signup", userData)
+    return axios.post(`${urlPrefix}/api/users/signup`, userData)
   },
 
   userLogin: function (loginData) {
-    return axios.post("http://localhost:8080/api/users/login", loginData, {withCredentials: true})
+    return axios.post(`${urlPrefix}/api/users/login`, loginData, {withCredentials: true})
   },
 
   getCurrentUser: function() {
-    return axios.get("http://localhost:8080/api/users/readsessions", {withCredentials: true})
+    return axios.get(`${urlPrefix}/api/users/readsessions`, {withCredentials: true})
   }
 };
 

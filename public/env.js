f7c71ef0954df7de015f4edf36fea8d333e3eb8f
James Fisher <jfisher396@hotmail.com>
2021-05-14 21:12:23 -0700

    added env variables to whitelist

--- public/env.js
+++ public/env.js
@@ -1,3 +1,4 @@
 window.env = {
-  "API_URL_PREFIX": "http://localhost:8080"
+  "API_URL_PREFIX_LOCAL": "http://localhost:8080",
+  "API_URL_PREFIX_DEPLOYED": "https://av-rental-2-api.herokuapp.com"
 };

dc60eb9aa621f582bd1293c86ee16f8f7cd5512e
James Fisher <jfisher396@hotmail.com>
2021-06-01 14:52:18 -0700

    main page loads upon registering

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import Container from "../../components/Container/Container";
 import SignupForm from "../../components/SignupForm/SignupForm";
 import API from "../../utils/API";
@@ -7,8 +8,8 @@ function Register() {
   const [registerData, setRegisterData] = useState({
     email: "",
     password: "",
-  })
-  
+  });
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setRegisterData({
@@ -17,11 +18,12 @@ function Register() {
     });
   };
 
+  let history = useHistory();
+
   const handleSubmitButton = (event) => {
     event.preventDefault();
-    
     if (registerData.email && registerData.password) {
-      API.newUser(this.state)
+      API.newUser(registerData)
         .then(() => {
           setRegisterData({
             email: "",
@@ -30,7 +32,7 @@ function Register() {
           alert(
             "Thank you for registering! Please login at the top of the page."
           );
-          
+          history.push("/");
         })
         .catch((err) => {
           alert("Registration failed. Email already in use.");
@@ -44,21 +46,19 @@ function Register() {
     }
   };
 
-  
-    return (
-      <div>
-        <Container>
-          <h1>Registration</h1>
-          <SignupForm
-            handleInputChange={handleInputChange}
-            handleSubmit={handleSubmitButton}
-            newUserEmail={registerData.email}
-            newUserPassword={registerData.password}
-          />
-        </Container>
-      </div>
-    );
-  }
-
+  return (
+    <div>
+      <Container>
+        <h1>Registration</h1>
+        <SignupForm
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmitButton}
+          newUserEmail={registerData.email}
+          newUserPassword={registerData.password}
+        />
+      </Container>
+    </div>
+  );
+}
 
 export default Register;
